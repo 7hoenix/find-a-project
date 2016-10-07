cb96a0a39c16cde4664275c66d0e14af77752ea1
Eric Meyer <emeyer@8thlight.com>
2016-10-07 21:17:21 +0000

    Add a seed script

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+projects = [{
+  description: "Working on iOS",
+  member_names: ["Eric", "Cyrus"]
+}, {
+  description: "Learning Clojure",
+  member_names: ["Justin"]
+}, {
+  description: "Wathing Clean Coders Videos",
+  member_names: ["Damon", "Eva", "Colin"]
+}]
+
+projects.each do |project_attributes|
+  project = Project.create!(description: project_attributes[:description])
+  project_attributes[:member_names].each do |member_name|
+    project.project_memberships.build(user_name: member_name)
+  end
+  project.save!
+end
