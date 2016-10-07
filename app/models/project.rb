8e1f80a0acebeb7fbf7996f2e11f69fb0472f1c5
Justin Holzmann <jholzmann@8thlight.com>
2016-10-07 14:29:59 -0500

    Add project memberships

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,7 @@
 class Project < ApplicationRecord
   has_many :project_memberships
+
+  def member_names
+    project_memberships.map(&:user_name)
+  end
 end
--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -9,5 +9,30 @@ describe Project do
     expect(project.description).to eq("working on iOS")
   end
 
+  describe "Project membership names" do
+    it "has none for no project memberships" do
+      project = Project.create!
+      expect(project.member_names).to eq([])
+    end
+
+    it "has a name for project memberships" do
+      project = Project.create!
+
+      project.project_memberships.create!(user_name: "Justin")
+
+      expect(project.member_names).to eq(["Justin"])
+    end
+
+    it "has many names for project memberships" do
+      project = Project.create!
+
+      project.project_memberships.build(user_name: "Justin")
+      project.project_memberships.build(user_name: "Eric")
+      project.save!
+
+      expect(project.member_names).to eq(["Justin", "Eric"])
+    end
+  end
+
 end
 
